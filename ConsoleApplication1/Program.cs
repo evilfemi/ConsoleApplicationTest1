d3249b362fafd38b6a54ef627aac4dc69f5d99af
Femi Alashi <femisimon@gmail.com>
2016-11-09 11:26:27 -0800

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Another console app for test
+            Console.WriteLine("This is another console app for test");
+            Console.Read();
+        }
+    }
+}
